150f43be468b07fa51eef91cd2e7f00635e16af9
SunilKumarr1234 <sunil414319@gmail.com>
2025-03-15 18:34:24 -0400

    Fixed paths and improved download logic

--- src/components/PicoCTF.jsx
+++ src/components/PicoCTF.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { FaDownload } from 'react-icons/fa' // Download Icon
+import { FaDownload } from 'react-icons/fa'
 import Header from './Header'
 import Footer from './Footer'
 
@@ -8,7 +8,7 @@ const PicoCTF = () => {
 
   const downloadPDF = (category) => {
     const fileName = `${category}.pdf`
-    const pdfPath = `${import.meta.env.BASE_URL}pdfs/${fileName}` // ✅ Corrected path for GitHub Pages
+    const pdfPath = `/pdfs/${fileName}` // ✅ Corrected Path
     const link = document.createElement('a')
     link.href = pdfPath
     link.download = fileName
@@ -20,8 +20,6 @@ const PicoCTF = () => {
       <Header />
       <div className='font-mono text-white px-6 sm:px-14 mt-10 pb-32'>
         <ul className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-10 gap-x-6'>
-
-          {/* Category Component */}
           {[
             { name: 'General Skills', pdf: 'General_Skills' },
             { name: 'Forensics', pdf: 'Forensics' },
@@ -30,11 +28,7 @@ const PicoCTF = () => {
             { name: 'Cryptography', pdf: 'Cryptography' },
             { name: 'Binary Exploitation', pdf: 'Binary_Exploitation' }
           ].map((category, index) => (
-            <li
-              key={index}
-              className='bg-gray-700 bg-opacity-10 rounded-lg p-4 relative'
-            >
-              {/* Download Button Positioned at Top-Right */}
+            <li key={index} className='bg-gray-700 bg-opacity-10 rounded-lg p-4 relative'>
               <button
                 className='absolute top-2 right-2 text-green-500 hover:text-green-400 transition-all duration-300'
                 onClick={() => downloadPDF(category.pdf)}
@@ -52,12 +46,12 @@ const PicoCTF = () => {
                 {category.name === 'Cryptography' ? (
                   <>
                     <li className='hover:text-green-500'>
-                      <Link to='https://play.picoctf.org/practice/challenge/351?category=2&page=1'>
+                      <Link to='https://play.picoctf.org/practice/challenge/351?category=2&page=1' target='_blank'>
                         ➤ HideToSee
                       </Link>
                     </li>
                     <li className='hover:text-green-500'>
-                      <Link to='https://play.picoctf.org/practice/challenge/418?category=2&page=1'>
+                      <Link to='https://play.picoctf.org/practice/challenge/418?category=2&page=1' target='_blank'>
                         ➤ interencdec
                       </Link>
                     </li>
